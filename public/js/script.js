2cc73f8b75ef464130010299bba0a7b88b0d93ba
Reiq.Laptop <reiqvideos@gmail.com>
2021-08-29 00:02:45 -0400

    Estilos del header y algunos del main

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,7 @@
 const socket = io(); //En caso de tener un dominio, definirlo como parametro, ejemplo: io('https://bit.ly/hernanreiq');
+
+//DOM
+var btn_send = document.getElementById('send');
+var message = document.getElementById('message');
+var actions = document.getElementById('actions');
+var output = document.getElementById('output');
